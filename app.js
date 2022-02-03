0a15000e309855531e0b66dc690212b6e4702146
Ricky <ochor01@pfw.edu>
2022-02-03 17:08:22 -0500

    Updated app.js
    Updated static css page
    Updated POST method for input variables

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const request = require('request')
+const https = require("https");
 const port = 3000
 
+//specifies a static folder
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/",function(req,res){
+
+    const email = req.body.email;
+    const firstName = req.body.firstName;
+    const secondName = req.body.secondName;
+    
+    console.log(firstName, secondName, email);
+    
+
+})
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + "/signup.html");
 })
 
 app.listen(port, () => {
