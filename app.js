66e61f638f5a53bc23c28880ee5729b8945cca65
Ricky <ochor01@pfw.edu>
2022-05-31 22:52:09 -0400

    First Commit

--- app.js
+++ app.js
@@ -1,69 +1,91 @@
 
 
-var bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const request = require('request')
-const https = require("https");
-const port = 3000
+var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+var path = require('path');
 
-//specifies a static folder
-app.use(express.static('public'))
+const request = require('request');
+const https = require("https");
+
+
+
+//provides a path  to our a static files for nodemon local host 3000
+app.use(express.static("public"));
+
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.post("/",function(req,res){
+
+app.post("/", function(req,res){
 
     const email = req.body.email;
-    const firstName = req.body.firstName;
-    const secondName = req.body.secondName;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
 
-    console.log(firstName, secondName, email);
+    console.log(firstName, lastName, email);
     
     var data = {
       members: [
         {
-          email_address:email,
+          email_address: email,
           status: "subscribed",
           merge_fields:{
             FNAME: firstName,
-            LNAME: secondName
+            LNAME: lastName
           }
         }
       ]
     };
 
+
+    //Turning JS data into a flatpack JSON
     //this will be sent to mail chimp 
     var jsonData = JSON.stringify(data);
 
 
-    const url = "https://$API_SERVER.api.mailchimp.com/3.0/lists/310eed242b";
+    const url = "https://us14.api.mailchimp.com/3.0/lists/310eed242b";
 
     const options = {
       method:"POST",
-      auth:"ricky:60f4c9c7182927661f2a7c28c70e346a-us14"
-    }
+      auth:"ricky:0f4c9c7182927661f2a7c28c70e346a-us14"
+    };
 
    const request = https.request(url, options ,function(response){
+
+        if(response.statusCode === 200){
+          res.sendFile(path.join(__dirname, '/success.html'));
+        }else{
+          res.sendFile(path.join(__dirname, '/failure.html'));
+        }
+
         response.on("data",function(data){
-          console.log(JSON.parse(data))
+          console.log(JSON.parse(data));
         })
-    })
+    });
 
-
+    //write to the monkey server 
     request.write(jsonData);
     request.end();
 
 });
 
-
+//specify route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/signup.html");
-})
+});
 
-app.listen(port, () => {
+//Dynamic port that Heroku will define on the go
+// process.env.PORT
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
+
+
+app.post('/failure.html', function(req,res){
+  res.redirect("/");
+
+});
 
 
 //mail chimp API key
